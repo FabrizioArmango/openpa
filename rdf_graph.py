8d6627f898c7baa1359a43dc2ccd592ebc0898ef
Elsinor <kr3at00r@gmail.com>
2017-10-22 16:22:12 +0200

    Update rdf_graph.py

--- rdf_graph.py
+++ rdf_graph.py
@@ -6,7 +6,7 @@ from rdflib import Graph, Literal, Namespace, URIRef
 from rdflib.namespace import RDF, RDFS, FOAF, OWL, XSD, DC, DCTERMS
 import requests
 
-API_URL = "http://palermo.linked-data.eu/php/PalermoAbout2.php";
+API_URL = "http://localhost/php/PalermoAbout2.php";
 def urify(ns, testo):
     testo=testo.replace(" ","_").replace(".","")
     return ns+urllib.parse.quote(testo)
@@ -50,17 +50,17 @@ g.bind("pldo", pldo)
 
 # FOOD
 # Non adoperiamo nessun tipo di modifica\bonifica\link a questo dato di dbpedia, dobbiamo lasciarlo ?
-for data_record in foodXML:
-    foodName = data_record.find("NAME")
-    enName = foodName.find("en").text
-    print(enName)
-    foodURI = urify("http://dbpedia.org/resource/", enName)
-    gs = g.resource(foodURI)
-    gs.set(RDF.type, pldo.CiboTipico)
+#for data_record in foodXML:
+#    foodName = data_record.find("NAME")
+#    enName = foodName.find("en").text
+#    print(enName)
+#    foodURI = urify("http://dbpedia.org/resource/", enName)
+#    gs = g.resource(foodURI)
+#    gs.set(RDF.type, pldo.CiboTipico)
 
     #for name in foodName:
     #    gs.add(pldo.nome, Literal(name.text, lang=name.tag))
-    gs.set(pldo.nome, Literal(enName, lang="en"))
+#    gs.set(pldo.nome, Literal(enName, lang="en"))
 
     #gs.set(pldo.immagine, URIRef(data_record.find("IMAGE").text))
 
@@ -72,6 +72,7 @@ for data_record in foodXML:
 for data_record in churchXML:
     churchName = data_record.find("NAME")    
     enName = churchName.find("en").text
+    enTag = churchName.find("en").tag
     print(enName)
     churchURI = urify("http://palermo.linked-data.eu/", enName)
     gs = g.resource(churchURI)
@@ -79,21 +80,22 @@ for data_record in churchXML:
     
     #for name in churchName:
     #    gs.add(pldo.nome, Literal(name.text, lang=name.tag))
-    gs.set(pldo.nome, Literal(enName, lang="en"))
+    gs.set(pldo.nome, Literal(enName, lang=enTag))
     
 
     #gs.set(pldo.immagine, URIRef(data_record.find("IMAGE").text))
-    location = data_record.find("LOCATION")
-    if location.find("LAT") is not None:
-        if location.find("LNG") is not None:
-            lat = location.find("LAT").text
-            lng = location.find("LNG").text
-            nearByChurchURL = "http://palermo.linked-data.eu:3000/?lat="+lat+"&lng="+lng
-            result = requests.get(nearByChurchURL).json()
-            for data in result:
-                gs.add(pldo.vicinoA, URIRef(data))
+    if data_record.find("LOCATION") is not None:
+        location = data_record.find("LOCATION")
+        if location.find("LAT") is not None:
+            if location.find("LNG") is not None:
+                lat = location.find("LAT").text
+                lng = location.find("LNG").text
+                nearByChurchURL = "http://localhost:3000/?lat="+lat+"&lng="+lng
+                result = requests.get(nearByChurchURL).json()
+                for data in result:
+                    gs.add(pldo.vicinoA, URIRef(data))
 
-    dbPediaChurchURI = urify("http://dbpedia.org/resource/", churchName.find("en").text)
+    dbPediaChurchURI = urify("http://dbpedia.org/resource/", enName)
     gs.set(OWL.sameAs, URIRef(dbPediaChurchURI))
     
 
@@ -131,7 +133,7 @@ for place in placeList:
                     gs.set(geo.lng, Literal(location.find("LNG").text, datatype=XSD.decimal))
                     lat = location.find("LAT").text
                     lng = location.find("LNG").text
-                    nearByPlaceURL = "http://palermo.linked-data.eu:3000/?lat="+lat+"&lng="+lng
+                    nearByPlaceURL = "http://localhost:3000/?lat="+lat+"&lng="+lng
                     result = requests.get(nearByPlaceURL).json()
                     for data in result:
                         gs.add(pldo.vicinoA, URIRef(data))
@@ -161,7 +163,7 @@ for data_record in theatreXML:
                 gs.set(geo.lng, Literal(location.find("LNG").text, datatype=XSD.decimal))        
                 lat = location.find("LAT").text
                 lng = location.find("LNG").text
-                nearByTheatresURL = "http://palermo.linked-data.eu:3000/?lat="+lat+"&lng="+lng
+                nearByTheatresURL = "http://localhost:3000/?lat="+lat+"&lng="+lng
                 result = requests.get(nearByTheatresURL).json()
                 for data in result:
                     gs.add(pldo.vicinoA, URIRef(data))
